60e4d8574a093a95dffda73f927d2d899761d65e
mauriciomarco <mauricio@chapman.edu>
2022-07-22 03:32:57 -0700

    [FIX] & condition

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # CPSC_380_Assignment_1
 The  objective  of  this  code  consists  of  designing  a  C/C++  program  to  serve  as  a  shell  interface that accepts user commands and then executes each command in a separate process.
+
+## Idendifying Information
+
+	* Name: Marco Mauricio
+	* StudentID: 2344979
+	* Email: mauricio@chapman.edu
+	* Course: CPSC 380
+	* Assignment: 1
+
+## Source Files
+
+	* sshell.c
+	
+## References
+
+	* https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
+	* https://www.ibm.com/docs/en/i/7.4?topic=functions-fgets-read-string
+	* https://beginnersbook.com/2014/01/c-strings-string-functions/
+	* https://linux.die.net/man/2/waitpid 
+	
+## Known Errors
+
+	* "&" command not recognized (i.e "ls -l &" is seen as garbage option
+
+## Build Instructions
+
+	* <code> gcc sshell.c -o sshell </code>
+	
+## Execution Instructions
+
+	* <code> ./sshell </code>
--- sshell.c
+++ sshell.c
@@ -1,24 +1,77 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 #define MAX_LINE 80 /* The maximum length command */
+
+			 /*
+			 * After reading user input, the steps are:
+			 * (1) fork a child process using fork()
+			 * (2) the child process will invoke execvp()
+		     * (3) if command included &, parent will invoke wait()
+             */
 
 int main(void)
 {
 	char *args[MAX_LINE/2 + 1]; /* command line arguments */
+	char line[MAX_LINE]; /* get command line */
+	char *token; /* piece of input string */
+	const char *delim = " "; /* param for strtok */
+	int i = 0;
+	int j;
+	int status;
+	int exit_code;
 	int should_run = 1; /* flag to determine when to exit program */
 	
-		while (should_run) {
-			printf("osh>");
-			fflush(stdout);
-			
-			/**
-			 * After reading user input, the steps are:
-			 * (1) for a child process using fork()
-			 * (2) the child process will invoke execvp()
-		     * (3) if command included &, parent will invoke wait()
-             */
+	while (should_run) {
+		printf("osh>");
+		fflush(stdout);
+
+		fgets(line, MAX_LINE, stdin); /* read input from command line */
+		size_t len = strlen(line);
+		if (line[len -1] == '\n'){ /* if input is new line */
+			line[len - 1] = '\0';
 		}
+		if (strcmp(line, "exit") == 0) { /* checking exit keyword */
+			break;
+		}
+		pid_t pid, wait_pid;
+		pid = fork(); /* start fork */
 		
-		 return 0;
+		switch(pid) /* handle child/parent processes */
+		{
+		case -1:
+			perror("fork failed... exiting");
+			exit(0);
+		case 0: /* child process */
+			token = strtok(line, delim); /* tokenize first command */
+			while (token != NULL) {
+				args[i] = token;
+				token = strtok(NULL, delim); /* tokenize again to get rest of commands */
+				i++; 
+			}
+			args[i] = NULL; /* set last to NULL */
+			execvp(args[0], args); 
+		default: /* parent process */
+			if (line[strlen(line) - 1] == '&') { /* checking & condition */
+				printf("wait command\n");
+				for (; j < i; j++) {
+					sleep(1);
+				}
+				wait_pid = wait(&status);
+				printf("Child has finished: PID = %d\n", wait_pid);
+				if (WIFEXITED(status)) {
+					printf("Child exited with code %d\n", WEXITSTATUS(status));
+				}
+				else {
+					printf("Child terminated abnormally\n");
+				}
+			}
+			exit(exit_code);
+		}
+	}
+	return 0;
 }
